e397676033c2fd2fb15e42bb5f7d52c8b3526ddd
david-dom <113909984+david-dom@users.noreply.github.com>
2023-09-11 00:03:59 -0500

    My commit message

--- src/agua/pages/screen1.jsx
+++ src/agua/pages/screen1.jsx
@@ -9,15 +9,45 @@ function Screen1() {
     
 
     return(
-        <div className={"baseScreen " }>
+        <div className={"baseScreen " + styles["Screen1"]}>
             
-<div id="layer-1-14691aa1a80f41c7ab8c6199acfaef8b" className={"baseLayer " + styles["layer-1-14691aa1a80f41c7ab8c6199acfaef8b"]}   >
-<div id="layer-2-7afde308669f428fbbe548a24b5ab12b" className={"baseLayer " + styles["layer-2-7afde308669f428fbbe548a24b5ab12b"]}   >
-<div id="layer-3-d1e4cd1fb5144fb6bd041a98fc748728" className={"baseLayer " + styles["layer-3-d1e4cd1fb5144fb6bd041a98fc748728"]}   >
-<div id="layer-4-1769119069e248dfaf38ed1a462f2c8d" className={"baseLayer " + styles["layer-4-1769119069e248dfaf38ed1a462f2c8d"]}   ></div>
-<span id="c78cef7ac2144e21b8c133cacca1ec12" className={"baseIcon " }  >menu</span>
+<div id="layer-1-45a10a88cf70400bb00beded167c5ccc" className={"baseLayer " + styles["layer-1-45a10a88cf70400bb00beded167c5ccc"]}   >
+<div id="layer-17-77608a89e7f143618c5d94be2c84cf59" className={"baseLayer " + styles["layer-17-77608a89e7f143618c5d94be2c84cf59"]}   >
+<div id="layer-19-2eaaf084773a448ea72194dc8af02afe" className={"baseLayer " + styles["layer-19-2eaaf084773a448ea72194dc8af02afe"]}   ></div>
+<span id="46175942b5eb44b39cb2d3e09b003bae" className={"baseIcon " + styles["icon-1-46175942b5eb44b39cb2d3e09b003bae"]}  >menu</span>
+</div>
+<div id="layer-18-972a83d2cc8e4c7f81baedf25452a512" className={"baseLayer " + styles["layer-18-972a83d2cc8e4c7f81baedf25452a512"]}   >
+<div id="layer-20-1227f11f739c4bb287716960ff3b81ce" className={"baseLayer " + styles["layer-20-1227f11f739c4bb287716960ff3b81ce"]}   >
+<div id="layer-21-dba7087fdb9b4557840b4fbeadb332b8" className={"baseLayer " + styles["layer-21-dba7087fdb9b4557840b4fbeadb332b8"]}   >
+<div id="text-1-3285cd9cb9ff4957bcd7dcbbbc8f8b82" className={"baseText " + styles["text-1-3285cd9cb9ff4957bcd7dcbbbc8f8b82"]}   >[Text]</div>
+<span id="3ab20402bab747709840af7e5770adea" className={"baseIcon " + styles["icon-2-3ab20402bab747709840af7e5770adea"]}  >extension</span>
+</div>
+<div id="layer-22-ff4055fc15734d03bba65c0705e3facd" className={"baseLayer " + styles["layer-22-ff4055fc15734d03bba65c0705e3facd"]}   >
+<div id="text-2-cfec73fbc57a4c238869e288dc336fed" className={"baseText " + styles["text-2-cfec73fbc57a4c238869e288dc336fed"]}   >[Text]</div>
+</div>
+<div id="layer-23-60d8bac0cce8474fa29bc96db38e55bd" className={"baseLayer " + styles["layer-23-60d8bac0cce8474fa29bc96db38e55bd"]}   >
+<div id="text-3-86f73e91e8174ea7b3ac412a63082772" className={"baseText " + styles["text-3-86f73e91e8174ea7b3ac412a63082772"]}   >[Text]</div>
 </div>
 </div>
+</div>
+</div>
+<div id="layer-2-fca5150bad28425c96891883f1961bc5" className={"baseLayer " + styles["layer-2-fca5150bad28425c96891883f1961bc5"]}   >
+<div id="layer-3-747d490938134c868279bf1358fb72b6" className={"baseLayer " + styles["layer-3-747d490938134c868279bf1358fb72b6"]}   ></div>
+<div id="layer-4-c05e64744106454fba158c969644f86b" className={"baseLayer " + styles["layer-4-c05e64744106454fba158c969644f86b"]}   >
+<div id="layer-6-535d9c8896264fdfaa4e251462fc9d27" className={"baseLayer " + styles["layer-6-535d9c8896264fdfaa4e251462fc9d27"]}   >
+<div id="text-4-2750cdc08d2c482fa2702edd3054b397" className={"baseText " + styles["text-4-2750cdc08d2c482fa2702edd3054b397"]}   >El app de carpooling para comunidades.</div>
+</div>
+<div id="layer-7-d21cbf44cad4457283d3409c2b9e8992" className={"baseLayer " + styles["layer-7-d21cbf44cad4457283d3409c2b9e8992"]}   ></div>
+<div id="layer-8-e709138f304240f6a126efa3c8a24c2f" className={"baseLayer " + styles["layer-8-e709138f304240f6a126efa3c8a24c2f"]}   >
+<div id="layer-12-7a799a18b972412890cc8a85d22c1101" className={"baseLayer " + styles["layer-12-7a799a18b972412890cc8a85d22c1101"]}   >
+<div id="layer-14-953b92dfc8074c11ac23a066d957f37f" className={"baseLayer " + styles["layer-14-953b92dfc8074c11ac23a066d957f37f"]}   ></div>
+<div id="layer-15-f2192de57c334072821566b935f4a2d3" className={"baseLayer " + styles["layer-15-f2192de57c334072821566b935f4a2d3"]}   ></div>
+<div id="layer-16-bb125fa54c5d4623a3229fdff3738324" className={"baseLayer " + styles["layer-16-bb125fa54c5d4623a3229fdff3738324"]}   ></div>
+</div>
+</div>
+<div id="layer-11-3da2d4a4b6f845849cd5cb16b6a923e4" className={"baseLayer " + styles["layer-11-3da2d4a4b6f845849cd5cb16b6a923e4"]}   ></div>
+</div>
+<div id="layer-5-f32ee8dae55c49d3967647a3f2738445" className={"baseLayer " + styles["layer-5-f32ee8dae55c49d3967647a3f2738445"]}   ></div>
 </div>
         </div>
     );
