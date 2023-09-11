9a561f97c656cc47aa9edc85b9ef38d8351e9d86
david-dom <113909984+david-dom@users.noreply.github.com>
2023-09-10 20:12:03 -0500

    My commit message

---
+++ src/agua/pages/screen1.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './screen1.module.css';
+
+
+
+function Screen1() {
+    
+    
+
+    return(
+        <div className={"baseScreen " }>
+            
+        </div>
+    );
+}
+
+export default Screen1;
+            
