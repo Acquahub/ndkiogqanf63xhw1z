f47ec486ac1b8df0685ae83af2675d0d29d76ff0
david-dom <113909984+david-dom@users.noreply.github.com>
2023-09-10 21:12:14 -0500

    My commit message

--- src/agua/pages/screen1.jsx
+++ src/agua/pages/screen1.jsx
@@ -11,6 +11,24 @@ function Screen1() {
     return(
         <div className={"baseScreen " }>
             
+<div id="layer-1-31db29044ab64d7b9044fc3a2d40f284" className={"baseLayer " + styles["layer-1-31db29044ab64d7b9044fc3a2d40f284"]}   >
+<div id="layer-2-998fa89af8f743ad933c6bd716588fee" className={"baseLayer " + styles["layer-2-998fa89af8f743ad933c6bd716588fee"]+ ' layer-2-998fa89af8f743ad933c6bd716588fee-orientation'}   >
+<div id="layer-3-fb94fc29dc874485ac1ae87804edceb5" className={"baseLayer " + styles["layer-3-fb94fc29dc874485ac1ae87804edceb5"]}   ></div>
+<div id="layer-4-b36b6c98da82492d939154ec936a1ebb" className={"baseLayer " + styles["layer-4-b36b6c98da82492d939154ec936a1ebb"]+ ' layer-4-b36b6c98da82492d939154ec936a1ebb-parentOrientationVariable'}   ></div>
+<div id="layer-5-7cbda320cf4f40c78f3e63211ab6bde3" className={"baseLayer " + styles["layer-5-7cbda320cf4f40c78f3e63211ab6bde3"]+ ' layer-5-7cbda320cf4f40c78f3e63211ab6bde3-orientation'}   >
+<div id="layer-6-0a6b50e3030a489586aeee361485d580" className={"baseLayer " + styles["layer-6-0a6b50e3030a489586aeee361485d580"]}   >
+<div id="text-1-3629ab737ecf4984bd1a8138be0297eb" className={"baseText " + styles["text-1-3629ab737ecf4984bd1a8138be0297eb"]}   >Quiénes somos</div>
+<span id="9875f494be9e4a079e3736d3e00f52b8" className={"baseIcon " + styles["icon-1-9875f494be9e4a079e3736d3e00f52b8"]}  >keyboard_arrow_down</span>
+</div>
+<div id="layer-7-5e9bccfb97454d349cf3e25cfe29c580" className={"baseLayer " + styles["layer-7-5e9bccfb97454d349cf3e25cfe29c580"]}   >
+<div id="text-2-692d9147f28844cba77e5196d724f803" className={"baseText " + styles["text-2-692d9147f28844cba77e5196d724f803"]}   >Empresas</div>
+</div>
+<div id="layer-8-5ff0d9064a694cc58a0a3510af6273cf" className={"baseLayer " + styles["layer-8-5ff0d9064a694cc58a0a3510af6273cf"]}   >
+<div id="text-3-b60d5e42c7954f96929e5bb7a406466a" className={"baseText " + styles["text-3-b60d5e42c7954f96929e5bb7a406466a"]}   >Descarga</div>
+</div>
+</div>
+</div>
+</div>
         </div>
     );
 }
